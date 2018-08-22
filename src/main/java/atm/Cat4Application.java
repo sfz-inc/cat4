f949e427e51f5ceaf639787b63757324a83e9771
Ryan Day <jrday@users.noreply.github.com>
2018-08-22 13:23:51 -0700

    Initial commit from Atomist

---
+++ src/main/java/atm/Cat4Application.java
@@ -0,0 +1,12 @@
+package atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cat4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cat4Application.class, args);
+	}
+}
